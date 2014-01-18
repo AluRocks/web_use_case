66bf91acaceca3e72e8949e83fad8deef7388eed
Alekhya <alekhya.rudra@cybrilla.com>
2014-01-18 11:21:11 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Promtcloud::Application.config.secret_token = '182e3849b05a3784b331ebdb63d700b238797537573abc3b3f993df23cb6e735afe442cf35a718e3d92cc5856ab42943fe1b6e1c42f5dcebccffedd9d599ffe6'
